c0972a3da0393d55be761d285f6460728f2219ba
niteshsalve1990-hue <niteshsalve1990@gmail.com>
2026-01-06 13:10:02 +0530

    swap
    007

---
+++ Swap.java
@@ -0,0 +1,14 @@
+package Session7;
+
+public class Swap
+{
+    public static void main(String[] args) {
+        int a=100;
+        int b=50;
+        a=a-b;
+        b=a+b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+
