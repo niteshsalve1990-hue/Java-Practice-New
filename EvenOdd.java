da5a0c422448b999a4c2b538bc51afb04c98a511
niteshsalve1990-hue <niteshsalve1990@gmail.com>
2026-01-06 13:08:38 +0530

    odd even
    007

---
+++ EvenOdd.java
@@ -0,0 +1,14 @@
+package Session7;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int a=200;
+        if(a%2==0){
+            System.out.println("its a even number");
+        }
+        else {
+            System.out.println("it is odd");
+        }
+
+    }
+}
