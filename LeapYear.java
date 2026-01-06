c58ad1a2beebd4fc30ddacf42aaccb30a9397ed4
niteshsalve1990-hue <niteshsalve1990@gmail.com>
2026-01-06 13:09:23 +0530

    leapyear
    007

---
+++ LeapYear.java
@@ -0,0 +1,14 @@
+package Session7;
+
+public class LeapYear {
+    public static void main(String[] args) {
+
+        int a=2024;
+        if(a%4==0){
+            System.out.println("is leap year");
+        }
+        else {
+            System.out.println("is not leap year");
+        }
+    }
+}
